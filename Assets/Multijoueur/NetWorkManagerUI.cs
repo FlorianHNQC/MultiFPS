7bc8c14433e11ea988c791c4f3a7cbd48f5989f5
R6Coach <barthelemy.mahe.pro@gmail.com>
2024-01-03 16:58:09 +0100

    menu start server

---
+++ Assets/Multijoueur/NetWorkManagerUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+
+public class NetWorkManagerUI : MonoBehaviour
+{
+    [SerializeField] private Button m_HostButton;
+    [SerializeField] private Button m_JoinButton;
+    [SerializeField] private Canvas m_Canvas;
+
+    private void Awake()
+    {
+        m_HostButton.onClick.AddListener(() =>
+        {
+            Debug.Log("start server");
+            NetworkManager.Singleton.StartHost();
+            m_Canvas.gameObject.SetActive(false);
+        });
+        m_JoinButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
+}
